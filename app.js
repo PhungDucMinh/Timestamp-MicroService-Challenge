15f087228b94ef8405e68b9e2c14c6d21bea364f
DucMinh <phungducminh95@gmail.com>
2017-01-08 02:09:10 +0700

    Update port

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(function(err, req, res, next) {
 });
 
 //module.exports = app;
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('Server is running at port: ' + port);
 });
